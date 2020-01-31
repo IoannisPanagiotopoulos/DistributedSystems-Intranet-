c2ee7b6b2cb35fa29442f833f7dc67a0d6d7e01d
mantzarisvas <mantzaris.vas98@gmail.com>
2020-01-31 20:47:19 +0200

    Updated Database schema

--- target/classes/db.schema.sql
+++ target/classes/db.schema.sql
@@ -3,24 +3,24 @@ CREATE TABLE `user` (
    `password` varchar(100) NOT NULL,
    `enabled` tinyint(4) NOT NULL,
    PRIMARY KEY (`username`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
  
- CREATE TABLE `department` (
+CREATE TABLE `department` (
    `department_name` enum('Informatics','Geography','Dietics','Economics') NOT NULL,
    `percentage` int(11) DEFAULT '0',
    `active` enum('active','inactive') DEFAULT 'inactive',
    PRIMARY KEY (`department_name`),
    UNIQUE KEY `department_name` (`department_name`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
  
- CREATE TABLE `authorities` (
+CREATE TABLE `authorities` (
    `username` varchar(50) NOT NULL,
    `authority` varchar(50) NOT NULL,
    UNIQUE KEY `ix_auth_username` (`username`,`authority`),
    CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
  
- CREATE TABLE `user_information` (
+CREATE TABLE `user_information` (
    `username` varchar(50) NOT NULL,
    `name` varchar(45) DEFAULT NULL,
    `email` varchar(45) DEFAULT NULL,
@@ -31,9 +31,9 @@ CREATE TABLE `user` (
    KEY `fk_user_information_department` (`department_name`),
    CONSTRAINT `fk_user_information_department` FOREIGN KEY (`department_name`) REFERENCES `department` (`department_name`),
    CONSTRAINT `fk_user_information_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
  
- CREATE TABLE `application` (
+CREATE TABLE `application` (
    `username` varchar(50) NOT NULL,
    `department_name` enum('Informatics','Geography','Dietics','Economics') DEFAULT NULL,
    `city` varchar(50) NOT NULL,
@@ -47,49 +47,12 @@ CREATE TABLE `user` (
    UNIQUE KEY `department_name` (`department_name`),
    CONSTRAINT `fk_application_department` FOREIGN KEY (`department_name`) REFERENCES `department` (`department_name`),
    CONSTRAINT `fk_application_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
- 
- 
-insert into user(username, password, enabled)
-values ('officer1', '1234', '1');
-insert into user(username, password, enabled)
-values ('officer2', '1234', '1');
-insert into user(username, password, enabled)
-values ('officer3', '1234', '1');
-insert into user(username, password, enabled)
-values ('student1', '1234', '1');
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-update user set password="$2y$12$pPFU7J.7MTmBjScWVV3./.eAtHduxk4kGEFEKAfjBjV42t7uXTqyy";
 
+insert into user(username, password, enabled) values ('root', '{bcryptedPassword}', 1);
+ 
 insert into department(department_name) values('Informatics');
 insert into department(department_name) values('Geography');
 insert into department(department_name) values('Dietics');
 insert into department(department_name) values('Economy');
-
-insert into authorities(username, authority)
-	values('officer1', 'ROLE_ADMIN');
-insert into authorities(username, authority)
-	values('officer2', 'ROLE_SUPERVISOR');
-insert into authorities(username, authority)
-	values('officer3', 'ROLE_OFFICER');
-insert into authorities(username, authority)
-	values('student1', 'ROLE_STUDENT');
-
-insert into user_information(username, name, email)
-	values ('officer1', 'root', 'r@r.r');
-insert into user_information(username, name, email)
-	values ('officer2', 'Anna', 'a@a.a');
-insert into user_information(username, name, email, department_name)
-	values ('officer3', 'Dimitra', 'd@d.d', 'Informatics');
-insert into user_information(username, name, email, department_name)
-	values ('student1', 'Vasilis', 'v@v.v', 'Informatics');
-	
-	
-insert into application (username, 
-department_name, 
-city, 
-personalIncome, 
-familyIncome, parent1_employmentStatus,
-parent2_employmentStatus,
-siblingsStudents) values
-('student1', 'Informatics', 'Athens', 0, 8000, 'emp', 'unemp', 1);
