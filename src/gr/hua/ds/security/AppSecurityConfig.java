7f96ca1fadfa2b0410b174842052810ced33c8fb
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2020-02-02 16:51:03 +0200

    Fixed case when student could log in

--- src/gr/hua/ds/security/AppSecurityConfig.java
+++ src/gr/hua/ds/security/AppSecurityConfig.java
@@ -27,7 +27,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
 				.usersByUsernameQuery("select username, password, enabled from user where username=?")
-				.authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+				.authoritiesByUsernameQuery("select username, authority from authorities where username=? "
+						+ "and authority!=ROLE_STUDENT");
 	}
 
 	@Configuration
